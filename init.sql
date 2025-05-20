c00c8845024cebe06376b0ea03bf7d7700c14318
florianK-717 <florian.kalinna@gmx.de>
2025-05-20 13:06:34 +0200

    Dont need Table all handled by framwork

--- init.sql
+++ init.sql
@@ -1,9 +1,3 @@
 -- pgvector-Erweiterung aktivieren
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- Beispiel-Tabelle für Embeddings
-CREATE TABLE IF NOT EXISTS documents (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    embedding VECTOR(1536)
-);
